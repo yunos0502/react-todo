d923d89c06c829c9f959ef3a256d2890df489b47
Park Gyu Ae <yunos0502@gmail.com>
2021-03-04 01:32:23 +0900

    create layout for todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const TodoDummyData = [
+  {
+    oneDepthId: 1,
+    oneDepthTitle: 'Buy',
+    twoDepth: [
+      {
+        twoDepthId: 1,
+        twoDepthTitle: 'Groceries',
+        threeDepth: [],
+      },
+      {
+        twoDepthId: 2,
+        twoDepthTitle: 'Outfit',
+        threeDepth: [
+          {
+            threeDepthId: 1,
+            threeDepthTitle: 'Apples',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    oneDepthId: 2,
+    oneDepthTitle: 'Study',
+    twoDepth: [
+      {
+        twoDepthId: 1,
+        twoDepthTitle: 'React',
+      },
+      {
+        twoDepthId: 2,
+        twoDepthTitle: 'Redux',
+        threeDepth: [
+          {
+            threeDepthId: 1,
+            threeDepthTitle: 'Action',
+          },
+          {
+            threeDepthId: 2,
+            threeDepthTitle: 'Reducer',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default function App() {
+  const [Text, setText] = useState('');
+
+  const changeText = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+  const onClickButton = () => {};
+
+  const TodoList = () => {
+    return TodoDummyData.map((firstData) => {
+      return (
+        <li key={firstData.oneDepthId}>
+          {firstData.oneDepthTitle}
+          <ul>
+            {firstData.twoDepth.map((secondData) => {
+              return (
+                <li key={secondData.twoDepthId}>
+                  {secondData.twoDepthTitle}
+                  <ul>
+                    {secondData?.threeDepth?.length > 0 &&
+                      secondData.threeDepth.map((thirdData) => (
+                        <li key={thirdData.threeDepthId}>
+                          {thirdData.threeDepthTitle}
+                        </li>
+                      ))}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </li>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>To-do</h1>
+      <div>
+        <input onChange={changeText} />
+        <input onChange={changeText} />
+        <input onChange={changeText} />
+        <button onClick={onClickButton}>확인</button>
+      </div>
+      <ul>{TodoList()}</ul>
     </div>
   );
 }
-
-export default App;
