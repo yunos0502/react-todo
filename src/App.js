ae322bae14dbca830dcf1a2aa8da350b622c9fde
Park Gyu Ae <yunos0502@gmail.com>
2021-03-19 23:09:29 +0900

    add todo styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styled from '@emotion/styled';
 
 export default function App() {
   const TodoDummyData = [
@@ -134,12 +135,56 @@ export default function App() {
 
   return (
     <div className='App'>
-      <h1>To-do</h1>
-      <input onChange={inputTodo} />
-      <button onClick={inputTodoHandler} data-depth="1">
-        확인
-      </button>
-      <ul>{todoList.length > 0 && Todos}</ul>
+      <Layout>
+        <h1>Todo List</h1>
+        <InputText>
+          <input onChange={inputTodo} />
+          <button onClick={inputTodoHandler} data-depth="1">
+            확인
+          </button>
+        </InputText>
+        <List>{todoList.length > 0 && Todos}</List>
+      </Layout>
     </div>
   );
 }
+
+const Layout = styled.div`
+  overflow: hidden;
+  margin: 30px auto;
+  padding: 0;
+  width: 100%;
+  max-width: 500px;
+
+  h1 {
+    text-align: center;
+  }
+`;
+
+const InputText = styled.div`
+  width: 95%;
+  display: flex;
+  height: 40px;
+  background-color: #ececec;
+  margin: 1rem auto;
+
+  input {
+    flex: 1;
+    border: 0;
+    background-color: #fff;
+    border: 1px solid #ececec;
+    padding: 0 1rem;
+  }
+  button {
+    width: 20%;
+    background-color: #ececec;
+  }
+`;
+
+const List = styled.ul`
+  border: 1px solid #ececec;
+  padding: 1rem;
+  /* box-shadow: 1px 1px 4px 0px rgb(0 0 0 / 20%); */
+  width: 95%;
+  margin: 0 auto 5px;
+`;
